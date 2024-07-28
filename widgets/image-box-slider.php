7fad738aedab575bce70a3b52b0f63b6f7409929
NazmulHasanWealcoder <142083558+NazmulHasanWealcoder@users.noreply.github.com>
2024-07-28 15:19:06 +0600

    added: update image box padding controls

--- widgets/image-box-slider.php
+++ widgets/image-box-slider.php
@@ -516,7 +516,7 @@ class Image_Box_Slider extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
+		$this->add_responsive_control(
 			'box_padding',
 			[
 				'label'      => esc_html__( 'Padding', 'animation-addons-for-elementor' ),
