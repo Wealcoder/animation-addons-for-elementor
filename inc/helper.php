58d1d19b3ab6b280293cdbdffba143931087342c
Rayhan Uddin <rayhanuddin.it.2019@gmail.com>
2024-11-14 17:02:19 +0600

    update

--- inc/helper.php
+++ inc/helper.php
@@ -323,4 +323,28 @@ if( !function_exists('wcf_get_search_active_keys') ) {
 	}
 }
 
+if(!function_exists('wcf_get_addon_active_extension_by_key')) {
+	
+	function wcf_get_addon_active_extension_by_key($search){
+	
+		$ext = get_option( 'wcf_save_extensions' );
+		if(is_array($ext)){
+		
+			$saved_ext  = array_keys( $ext );	
+			$found_key = array_search($search, $saved_ext);			
+			if($found_key !==false){
+				return true;
+			}
+			
+		}else{
+			return true;
+		}	
+		
+		return false;
+	}	
+
+}
+
+
+
 
