12a0977bb0b8bebd551a1df380dadfe99a8e7e11
Mizanur Rahaman <wealcode.mizan@gamil.com>
2024-11-14 14:58:11 +0600

    update

--- inc/admin/dashboard.php
+++ inc/admin/dashboard.php
@@ -246,9 +246,9 @@ class WCF_Admin_Init {
 			
 			$extensions = get_option( 'wcf_save_extensions' );
 			$saved_extensions = is_array($extensions) ? array_keys( $extensions ) : [];		  
-            wcf_get_search_active_keys($GLOBALS['wcf_addons_config']['extensions'], $saved_extensions, $foundext, $activeext);
-		    $active_widgets = self::get_widgets(); 
-		    $active_ext = self::get_extensions(); 
+      wcf_get_search_active_keys($GLOBALS['wcf_addons_config']['extensions'], $saved_extensions, $foundext, $activeext);
+		  $active_widgets = self::get_widgets(); 
+		  $active_ext = self::get_extensions(); 
 	
 			$localize_data = [
 				'ajaxurl'        => admin_url( 'admin-ajax.php' ),
