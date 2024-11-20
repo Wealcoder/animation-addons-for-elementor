6878cb086fe810cc0c65721c04ef23438f90e497
wealcodersk <jahid.wealcoder@gmail.com>
2024-11-20 17:20:27 +0600

    working on license

--- inc/admin/dashboard/src/components/header/MainHeader.jsx
+++ inc/admin/dashboard/src/components/header/MainHeader.jsx
@@ -1,14 +1,20 @@
-import { RiNotificationLine, RiSearchLine } from "react-icons/ri";
-import { Button } from "../ui/button";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiVipCrown2Line,
+} from "react-icons/ri";
+import { Button, buttonVariants } from "../ui/button";
 import MainNav from "./MainNav";
 import ShortLogo from "./ShortLogo";
 import { Badge } from "../ui/badge";
 import GlobalSearch from "../shared/GlobalSearch";
 import LicenseDialog from "../shared/LicenseDialog";
+import { cn } from "@/lib/utils";
 
 const MainHeader = ({ open, setOpen, NavigateComponent }) => {
   // console.log(WCF_ADDONS_ADMIN.addons_config);
-  console.log(WCF_ADDONS_ADMIN.addons_config.wcf_valid);
+  console.log(WCF_ADDONS_ADMIN.addons_config);
+  const activated = WCF_ADDONS_ADMIN.addons_config;
   return (
     <div className="flex justify-between items-center gap-6 py-5 px-8 border-b border-border-secondary">
       <div>
@@ -25,7 +31,31 @@ const MainHeader = ({ open, setOpen, NavigateComponent }) => {
           <Badge className="absolute top-[9px] right-2" variant="solid" />
           <RiNotificationLine size={20} />
         </Button>
-        <LicenseDialog />
+        {activated.integrations.plugins.elements[
+          "animation-addon-for-elementorpro"
+        ].action === "Active" ? (
+          <Button variant="pro">
+            <span className="me-2 flex">
+              <RiVipCrown2Line size={20} />
+            </span>
+            Active Plugin
+          </Button>
+        ) : activated.integrations.plugins.elements[
+            "animation-addon-for-elementorpro"
+          ].action === "Download" ? (
+          <a
+            href="https://animation-addons.com/"
+            className={cn(buttonVariants({ variant: "pro" }))}
+          >
+            <span className="me-2 flex">
+              <RiVipCrown2Line size={20} />
+            </span>
+            Get Pro Version
+          </a>
+        ) : (
+          <LicenseDialog />
+        )}
+
         {/* <Button variant="pro" disabled>
           <span className="me-2 flex">
             <RiVipCrown2Line size={20} />
