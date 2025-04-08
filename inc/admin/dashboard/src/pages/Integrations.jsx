b7620fcf9bad77b1a1ccf72a6d1bb3874ef08414
wealcodersk <jahid.wealcoder@gmail.com>
2025-04-08 15:16:05 +0600

    working on integration

--- inc/admin/dashboard/src/pages/Integrations.jsx
+++ inc/admin/dashboard/src/pages/Integrations.jsx
@@ -1,8 +1,15 @@
 import IntegrationTopBar from "@/components/integrations/IntegrationTopBar";
 import ShowIntegrations from "@/components/integrations/ShowIntegrations";
-import { buttonVariants } from "@/components/ui/button";
-import { PromotionList } from "@/config/data/promotionList";
-import { cn } from "@/lib/utils";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion2";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { Dot } from "lucide-react";
+import { Badge } from "../ui/badge";
 
 const Integrations = () => {
   return (
@@ -13,38 +20,169 @@ const Integrations = () => {
       <div className="mt-4">
         <ShowIntegrations />
 
-        {/* promotion  */}
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 mt-11">
-          {PromotionList?.map((item, i) => (
-            <div
-              key={`promotion-${i}`}
-              className="border rounded-2xl p-5 ps-6 flex flex-col gap-6 shadow-common"
-            >
-              <div>
-                <div>
-                  <img
-                    className="w-full h-full object-cover"
-                    src={item.image}
-                    alt="thumbnail"
+        {/* <div className="mt-11">
+          <div className="bg-background-secondary p-2.5 rounded-lg">
+            <div>
+              <div className="bg-background flex justify-between items-center p-5 rounded">
+                <h3 className="text-base font-medium">
+                  {filteredGsapExtensions?.title}
+                </h3>
+                <div className="flex items-center space-x-2">
+                  <Switch
+                    id={`enable-gsap`}
+                    checked={filteredGsapExtensions?.is_active}
+                    onCheckedChange={(value) =>
+                      updateActiveGsapAllExtension({ value })
+                    }
                   />
+                  <Label htmlFor={`enable-gsap`}>Enable All</Label>
                 </div>
               </div>
-              <div>
-                <h2 className="text-xl font-medium ">{item.title}</h2>
-                <p className="text-sm mt-[10px] text-text-secondary line-clamp-3">
-                  {item.description}
-                </p>
-                <a
-                  href={item.url}
-                  className={cn(buttonVariants(), "mt-7 px-11")}
-                  target="_blank"
-                >
-                  Buy Now
-                </a>
-              </div>
+
+              <Accordion
+                type="multiple"
+                value={openAccordion}
+                onValueChange={(value) => setOpenAccordion(value)}
+                className="w-full mt-2 space-y-1.5"
+              >
+                {Object.keys(filteredGsapExtensions?.elements)?.map(
+                  (extension) => (
+                    <AccordionItem key={extension} value={extension}>
+                      <div className="p-[2px]">
+                        <div className="flex items-center bg-background justify-between gap-3 py-3 px-4">
+                          <AccordionTrigger className="rounded cursor-pointer w-full">
+                            <div className="flex flex-col gap-1">
+                              <div className="text-[15px] leading-6 font-medium flex items-center">
+                                {
+                                  filteredGsapExtensions?.elements[extension]
+                                    .title
+                                }
+                                {filteredGsapExtensions?.elements[extension]
+                                  ?.is_pro ? (
+                                  <>
+                                    <Dot
+                                      className="w-3.5 h-3.5 text-icon-secondary"
+                                      strokeWidth={2}
+                                    />
+                                    <Badge variant="pro">PRO</Badge>
+                                  </>
+                                ) : (
+                                  ""
+                                )}
+                              </div>
+                            </div>
+                          </AccordionTrigger>
+                          <div className="flex gap-1 items-center">
+                            {Object.keys(
+                              filteredGsapExtensions?.elements[extension]
+                                ?.elements
+                            )?.length ? (
+                              ""
+                            ) : (
+                              <>
+                                <Badge
+                                  variant="pro"
+                                  className="px-2.5 py-1.5 h-7 bg-[linear-gradient(180deg,#FFA184_0%,#F2754F_100%)] mr-1"
+                                >
+                                  COMING SOON!
+                                </Badge>
+                              </>
+                            )}
+
+                            <Switch
+                              checked={
+                                filteredGsapExtensions?.elements[extension]
+                                  ?.is_active
+                              }
+                              onCheckedChange={(value) => {
+                                value
+                                  ? setOpenAccordion((prev) => [
+                                      ...prev,
+                                      extension,
+                                    ])
+                                  : setOpenAccordion((prev) =>
+                                      prev?.filter((el) => el !== extension)
+                                    );
+                                updateActiveGsapGroupExtension({
+                                  value,
+                                  slug: extension,
+                                });
+                              }}
+                              disabled={
+                                !Object.keys(
+                                  filteredGsapExtensions?.elements[extension]
+                                    ?.elements
+                                )?.length
+                              }
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <AccordionContent>
+                        <div className="grid grid-cols-2 xl:grid-cols-3 gap-1 mt-1 p-[2px]">
+                          {Object.keys(
+                            filteredGsapExtensions?.elements[extension]
+                              ?.elements
+                          )?.length ? (
+                            <>
+                              {Object.keys(
+                                filteredGsapExtensions?.elements[extension]
+                                  ?.elements
+                              )?.map((content, i) => (
+                                <React.Fragment key={`tab_content-${i}`}>
+                                  <WidgetCard
+                                    widget={
+                                      filteredGsapExtensions?.elements[
+                                        extension
+                                      ]?.elements[content]
+                                    }
+                                    slug={content}
+                                    updateActiveItem={updateActiveGsapExtension}
+                                    isDisable={
+                                      !filteredGsapExtensions?.elements[
+                                        extension
+                                      ]?.is_active
+                                    }
+                                    className="rounded p-5"
+                                  />
+                                </React.Fragment>
+                              ))}
+                              {Array.from({
+                                length:
+                                  deviceMediaMatch() -
+                                  (Object.keys(
+                                    filteredGsapExtensions?.elements[extension]
+                                      ?.elements
+                                  )?.length %
+                                    deviceMediaMatch() ===
+                                  0
+                                    ? deviceMediaMatch()
+                                    : Object.keys(
+                                        filteredGsapExtensions?.elements[
+                                          extension
+                                        ]?.elements
+                                      )?.length % deviceMediaMatch()),
+                              }).map((_, index) => (
+                                <WidgetCard
+                                  key={`tab_content_empty-${index}`}
+                                  className="rounded"
+                                />
+                              ))}
+                            </>
+                          ) : (
+                            <div className="col-span-3 px-4 py-[15px] bg-background rounded-lg  box-border">
+                              <p className="text-center">Coming soon...</p>
+                            </div>
+                          )}
+                        </div>
+                      </AccordionContent>
+                    </AccordionItem>
+                  )
+                )}
+              </Accordion>
             </div>
-          ))}
-        </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
