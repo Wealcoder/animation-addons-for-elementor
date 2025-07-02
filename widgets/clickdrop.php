4784c9a9e537c42030dac5cb43f329af3aa7626c
ahsan <ahsan.wealcoder@gmail.com>
2025-07-02 15:07:23 +0600

    widget files and markup ready to play

---
+++ widgets/clickdrop.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace WCF_ADDONS\Widgets;
+
+use Elementor\Controls_Manager;
+use Elementor\Plugin;
+use Elementor\Utils;
+use Elementor\Group_Control_Text_Stroke;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Text_Shadow;
+use Elementor\Widget_Base;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;   // Exit if accessed directly.
+}
+
+class ClickDrop extends Widget_Base {
+
+	public function get_name() {
+		return 'wcf--clickdrop';
+	}
+
+	public function get_title() {
+		return esc_html__( 'ClickDrop', 'animation-addons-for-elementor' );
+	}
+
+	public function get_icon() {
+		return 'wcf eicon-click';
+	}
+
+	public function show_in_panel() {
+		// By default don't show.
+		return true;
+	}
+
+	public function get_categories() {
+		return [ 'wcf-single-addon' ];
+	}
+
+	public function get_keywords() {
+		return ['clickdrop','popup', 'content popup' ];
+	}
+
+    public function get_style_depends() {
+        return [ 'aae-clickdrop' ];
+    }
+
+    public function get_script_depends() {
+        return [ 'aae-clickdrop' ];
+    }
+
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => esc_html__( 'Heading', 'animation-addons-for-elementor' ),
+			]
+		);
+
+
+
+		$this->end_controls_section();
+	}
+
+
+	protected function render() {
+
+        $settings = $this->get_settings_for_display();
+
+	}
+}
