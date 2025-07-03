946f31f56baed0aadadade11204206759735c9f4
ahsan <ahsan.wealcoder@gmail.com>
2025-07-03 13:00:22 +0600

    pull

--- assets/js/widgets/contact-form.js
+++ assets/js/widgets/contact-form.js
@@ -1,9 +1,10 @@
 (function waitForElementorReady() {
-  if (typeof window.elementorFrontend !== 'undefined' && elementorFrontend.hooks && typeof elementorFrontend.hooks.addAction === 'function') {
-    elementorFrontend.hooks.addAction('frontend/element_ready/wcf--contact-form-7.default', function ($scope) {
-      console.log('[Debug] A widget is ready');
-    });
-  } else {
-    setTimeout(waitForElementorReady, 100);
-  }
+	if (typeof window.elementorFrontend !== 'undefined' && elementorFrontend.hooks && typeof elementorFrontend.hooks.addAction === 'function') {
+		elementorFrontend.hooks.addAction('frontend/element_ready/wcf--contact-form-7.default', function($scope) {
+			console.log('[Debug] A widget is ready');
+		});
+	} else {
+		setTimeout(waitForElementorReady, 100);
+	}
 })();
+//# sourceMappingURL=contact-form.js.map
