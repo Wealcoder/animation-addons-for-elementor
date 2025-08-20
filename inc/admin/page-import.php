c69057ade2a91f6db2fdafa72fba287f539e5c56
Rayhan Uddin <rayhanuddin.it.2019@gmail.com>
2025-08-20 10:46:19 +0600

    update

--- inc/admin/page-import.php
+++ inc/admin/page-import.php
@@ -73,4 +73,28 @@ final class AAE_Admin_Buttons {
 if(is_admin()){
     new AAE_Admin_Buttons();
 }
+add_action('admin_head-edit.php', function () {
+    global $typenow;
+
+    if ($typenow !== 'page') {
+        return;
+    }
+
+    ?>
+    <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const heading = document.querySelector('.wrap .wp-heading-inline + .page-title-action');
+        if (heading) {
+            const btn = document.createElement('a');
+            btn.href = '#';
+            
+            btn.id = 'aae-heading-button';
+            btn.className = 'page-title-action'; // same styling as Add New
+            btn.innerText = 'Import Page';
+            heading.after(btn);
+        }
+    });
+    </script>
+    <?php
+});
 
