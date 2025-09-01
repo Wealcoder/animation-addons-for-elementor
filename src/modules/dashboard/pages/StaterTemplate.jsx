ddf888672af17e23d929eac39cbf40ce1e5469b9
Jahid Hasan <jahid.wealcoder@gmail.com>
2025-09-01 14:35:19 +0600

    update search

--- src/modules/dashboard/pages/StaterTemplate.jsx
+++ src/modules/dashboard/pages/StaterTemplate.jsx
@@ -1,47 +1,33 @@
-import TemplateLeftFilter from "@/components/template/left/TemplateLeftFilter";
 import TemplateRightContent from "@/components/template/right/TemplateRightContent";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useEffect, useRef, useState, useCallback } from "react";
 import { debounceFn } from "@/lib/utils";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet";
+
 import { StaterTemplateHeader } from "../components/header/StaterTemplateHeader";
-import { RiArrowLeftLine } from "react-icons/ri";
 import StarterTemplateFilter from "../components/header/StarterTemplateFilter";
+import TemplateSearch from "../components/template/TemplateSearch";
 
 const StaterTemplate = () => {
   const viewportRef = useRef(null);
-  const prevScrollTop = useRef(0); // Track previous scroll position
+  const prevScrollTop = useRef(0);
   const [hasReachedBottom, setHasReachedBottom] = useState(false);
-  const [searchKey, setSearchKey] = useState("");
-  const [filterKey, setFilterKey] = useState("");
+  const [metaData, setMetaData] = useState({
+    searchKey: "",
+    filterKey: "",
+    pageNum: 1,
+    types: [],
+    license: "",
+    selectedCategory: [],
+    wishlist: WCF_ADDONS_ADMIN.addons_config.wishlist.toString(),
+  });
   const [allTemplate, setAllTemplate] = useState({});
-  const [pageNum, setPageNum] = useState(1);
   const [loading, setLoading] = useState(true);
-  const [types, setTypes] = useState([]);
-  const [license, setLicense] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState([]);
-  const [openSidebar, setOpenSidebar] = useState(false);
 
   useEffect(() => {
-    const meta = {
-      searchKey,
-      filterKey,
-      pageNum,
-      types,
-      license,
-      selectedCategory,
-      allTemplate,
-      wishlist: WCF_ADDONS_ADMIN.addons_config.wishlist.toString(),
-    };
+    metaData.allTemplate = allTemplate;
 
-    getAllTemplate(meta);
-  }, [searchKey, filterKey, pageNum, types, license, selectedCategory]);
+    getAllTemplate(metaData);
+  }, [metaData]);
 
   const getAllTemplate = useCallback(
     debounceFn(async (meta) => {
@@ -131,7 +117,7 @@ const StaterTemplate = () => {
         allTemplate?.templates?.length !== allTemplate?.total
       ) {
         setHasReachedBottom(true);
-        setPageNum((prev) => prev + 1);
+        setMetaData((prev) => ({ ...prev, pageNum: prev.pageNum + 1 }));
       }
     };
 
@@ -174,28 +160,31 @@ const StaterTemplate = () => {
     <ScrollArea className="h-screen" ref={viewportRef}>
       <div className="px-5 py-[54px] min-h-screen relative starter-template-dashboard">
         <div className="fixed top-5 left-0 px-8 w-full z-50">
-          <div className="flex items-center gap-[30px] bg-white shadow-[0_0_15px_10px_rgba(0,0,0,0.05)] px-[25px] rounded-[10px] min-h-[74px]">
-            <div
-              onClick={() => changeRoute("dashboard")}
-              className="cursor-pointer flex w-11 h-11 px-[3px] py-1 justify-center items-center bg-[#F6502C] rounded-full"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-                fill="none"
-                className="w-[14px] h-[14px] flex-shrink-0"
+          <div className="flex justify-between items-center bg-white shadow-[0_0_15px_10px_rgba(0,0,0,0.05)] px-[25px] rounded-[10px] min-h-[74px]">
+            <div className="flex items-center gap-[30px]">
+              <div
+                onClick={() => changeRoute("dashboard")}
+                className="cursor-pointer flex w-11 h-11 px-[3px] py-1 justify-center items-center bg-[#F6502C] rounded-full"
               >
-                <path
-                  d="M3.34988 6.1001H14V7.90001H3.34988L8.04335 12.7273L6.80593 14L0 7.00005L6.80593 0L8.04335 1.27273L3.34988 6.1001Z"
-                  fill="#ffffff"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  className="w-[14px] h-[14px] flex-shrink-0"
+                >
+                  <path
+                    d="M3.34988 6.1001H14V7.90001H3.34988L8.04335 12.7273L6.80593 14L0 7.00005L6.80593 0L8.04335 1.27273L3.34988 6.1001Z"
+                    fill="#ffffff"
+                  />
+                </svg>
+              </div>
+              <StaterTemplateHeader />
+              <div className="bg-[#1212121A] w-[1px] h-6"></div>
+              <StarterTemplateFilter />
             </div>
-            <StaterTemplateHeader />
-            <div className="bg-[#1212121A] w-[1px] h-6"></div>
-            <StarterTemplateFilter />
+            <TemplateSearch setMetaData={setMetaData} />
           </div>
         </div>
         <div className="px-8 py-[80px] bg-[#F9F9F9] rounded-[30px]">
@@ -204,15 +193,7 @@ const StaterTemplate = () => {
               <p className="text-lg font-semibold">Loading...</p>
             </div>
           ) : (
-            <TemplateRightContent
-              searchKey={searchKey}
-              setSearchKey={setSearchKey}
-              filterKey={filterKey}
-              setFilterKey={setFilterKey}
-              setPageNum={setPageNum}
-              allTemplate={allTemplate}
-              setOpenSidebar={setOpenSidebar}
-            />
+            <TemplateRightContent allTemplate={allTemplate} />
           )}
           {loading && allTemplate?.templates?.length ? (
             <div className="flex justify-center items-center h-[25vh]">
