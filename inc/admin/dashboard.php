460ba831bc87f65bb88af7e5beac2dce3694bb62
Rayhan Uddin <rayhanuddin.it.2019@gmail.com>
2026-01-14 16:51:12 +0600

    popup

--- inc/admin/dashboard.php
+++ inc/admin/dashboard.php
@@ -382,11 +382,11 @@ class WCF_Admin_Init
 				'user_role'           => wcfaddon_get_current_user_roles(),
 				'version'             => WCF_ADDONS_VERSION,
 				'st_template_domain'  => WCF_TEMPLATE_STARTER_BASE_URL,
-				'home_url' => add_query_arg(['aae-cache' => 1], home_url('/')),
-				'template_menu' => $this->get_template_menu_data(),
-				'hero' => file_exists($this->plugin_file) ? WCF_ADDONS_URL . 'assets/images/hero-banner.jpg' : 'no',
-				'hero_offer' => WCF_ADDONS_URL . 'assets/video/cyber-sale.mp4',
-				'root_url' => WCF_ADDONS_URL,
+				'home_url'            => add_query_arg(['aae-cache' => 1], home_url('/')),
+				'template_menu'       => $this->get_template_menu_data(),
+				'hero'                => file_exists($this->plugin_file) ? WCF_ADDONS_URL . 'assets/images/hero-banner.jpg' : 'no',
+				'hero_offer'          => WCF_ADDONS_URL . 'assets/video/cyber-sale.mp4',
+				'root_url'            => WCF_ADDONS_URL,
 			);
 			wp_localize_script('wcf-admin', 'WCF_ADDONS_ADMIN', $localize_data);
 		}
