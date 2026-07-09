826ed758c818982def270ee9b8f950de92a8b7da
nayeemwealcoder <nayeem.wealcoder@gmail.com>
2026-07-09 12:32:23 +0600

    removed preset system from social wrap

--- inc/AtomicWidgets/Widgets/SocialWrap/class-aae-a-social-wrap.php
+++ inc/AtomicWidgets/Widgets/SocialWrap/class-aae-a-social-wrap.php
@@ -78,18 +78,7 @@ class AAE_A_Social_Wrap extends Atomic_Element_Base {
 	}
 
 	protected function define_atomic_controls(): array {
-		require_once __DIR__ . '/class-aae-a-preset-picker-control.php';
-
 		return [
-			Section::make()
-				->set_label( __( 'Presets', 'animation-addons-for-elementor' ) )
-				->set_id( 'aae_presets' )
-				->set_items( [
-					AAE_A_Preset_Picker_Control::make()
-						->set_label( __( 'Apply Preset', 'animation-addons-for-elementor' ) )
-						->set_meta( [ 'layout' => 'custom' ] ),
-				] ),
-
 			Section::make()
 				->set_label( __( 'Settings', 'animation-addons-for-elementor' ) )
 				->set_id( 'settings' )
